2b13aafba0eebb383c1006384a24d5ec05a83da1
randji <stephanesoilihi@yahoo.fr>
2024-06-20 21:50:00 +0200

    Ajout système routage

---
+++ Router/Route.js
@@ -0,0 +1,10 @@
+
+// Class Route
+export default class Route {
+    constructor(url, title, pathHtml, pathJS = "") {
+      this.url = url;
+      this.title = title;
+      this.pathHtml = pathHtml;
+      this.pathJS = pathJS;
+    }
+}
